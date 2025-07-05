cad1351c8b6b4e3cb03ba8656621806fca48afc6
Sohanur Rahman <sohanuractive007@gmail.com>
2025-07-06 03:31:46 +0600

    added purchase button functionality

--- client/src/components/Modal/PurchaseModal.jsx
+++ client/src/components/Modal/PurchaseModal.jsx
@@ -1,48 +1,92 @@
-import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import useAuth from "../../hooks/useAuth";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
-const PurchaseModal = ({ closeModal, isOpen }) => {
-  // Total Price Calculation
+const PurchaseModal = ({ closeModal, isOpen, plant }) => {
+  const { user } = useAuth();
+  const { name, category, quantity, price } = plant || {};
+
+  const [selectedQuantity, setSelectedQuantity] = useState(1);
+  const [selectedPrice, setSelectedPrice] = useState(price);
+
+  const handleQuantity = (value) => {
+    const totalQuantity = parseInt(value);
+    if (totalQuantity > quantity)
+      toast.error("No more Plant, please chose quantity based");
+    const calculatePrice = totalQuantity * price;
+    setSelectedPrice(calculatePrice);
+    setSelectedQuantity(totalQuantity);
+  };
 
   return (
     <Dialog
       open={isOpen}
-      as='div'
-      className='relative z-10 focus:outline-none '
+      as="div"
+      className="relative z-10 focus:outline-none "
       onClose={closeModal}
     >
-      <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
-        <div className='flex min-h-full items-center justify-center p-4'>
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4">
           <DialogPanel
             transition
-            className='w-full max-w-md bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0 shadow-xl rounded-2xl'
+            className="w-full max-w-md bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0 shadow-xl rounded-2xl"
           >
             <DialogTitle
-              as='h3'
-              className='text-lg font-medium text-center leading-6 text-gray-900'
+              as="h3"
+              className="text-lg font-medium text-center leading-6 text-gray-900"
             >
               Review Info Before Purchase
             </DialogTitle>
-            <div className='mt-2'>
-              <p className='text-sm text-gray-500'>Plant: Money Plant</p>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">Plant: {name}</p>
             </div>
-            <div className='mt-2'>
-              <p className='text-sm text-gray-500'>Category: Indoor</p>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">Category: {category}</p>
             </div>
-            <div className='mt-2'>
-              <p className='text-sm text-gray-500'>Customer: PH</p>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">
+                Customer: {user.displayName}
+              </p>
             </div>
 
-            <div className='mt-2'>
-              <p className='text-sm text-gray-500'>Price: $ 120</p>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">Price per unit: $ {price}</p>
             </div>
-            <div className='mt-2'>
-              <p className='text-sm text-gray-500'>Available Quantity: 5</p>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">
+                Available Quantity: {quantity}
+              </p>
+            </div>
+
+            <hr className="text-gray-400 mt-2" />
+            <p className="text-md text-gray-600">Order Info</p>
+
+            <div className="mt-2">
+              <input
+                // value={selectedQuantity}
+                onChange={(e) => handleQuantity(e.target.value)}
+                type="number"
+                min={1}
+                max={quantity}
+                className="border-gray-400 border"
+              />
+            </div>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">
+                Available Quantity: {selectedQuantity}
+              </p>
+            </div>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">
+                Total Price: {selectedPrice}
+              </p>
             </div>
           </DialogPanel>
         </div>
       </div>
     </Dialog>
-  )
-}
+  );
+};
 
-export default PurchaseModal
+export default PurchaseModal;
